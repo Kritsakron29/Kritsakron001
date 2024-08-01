54ed3d3b660ad0aff86873f6d2e0e0015f9c0d11
Kritsakron29 <152351348+Kritsakron29@users.noreply.github.com>
2024-08-01 10:12:22 +0700

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,14 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        for(int i =1; i <= 100; i++){
+            System.err.println(+i);
+
+        }
+
+        int iWhile = 1;
+        while (iWhile <= 100) {
+            System.err.println("result in whileloop : "+iWhile);
+            iWhile ++ ;
+        }
+    }
+}
